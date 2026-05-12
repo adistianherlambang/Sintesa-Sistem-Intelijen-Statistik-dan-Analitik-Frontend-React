4fd619c1218a86a68e0e73711879b0440471aa86
Adistian Herlambang <adistian59@gmail.com>
2026-05-12 16:09:04 +0700

    init dashboard

--- src/page/Dashboard/Dashboard.jsx
+++ src/page/Dashboard/Dashboard.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 import styles from "./Dashboard.module.css"
 
 import Logo from '../../components/Logo/Logo'
 
 export default function Dashboard() {
+
+  const location = useLocation() 
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <Logo/>
+        
       </div>
       <div className={styles.right}>
-        ah
+        <div style={{height: "200vh", backgroundColor: "blue"}}></div>
+        <div style={{height: "200vh", backgroundColor: "white"}}></div>
       </div>
     </div>
   )
