10bd3908a238c086ca59c14ebd8c79869fae4046
Adistian Herlambang <adistian59@gmail.com>
2026-05-12 14:12:55 +0700

    add logo

--- src/page/Dashboard/Dashboard.jsx
+++ src/page/Dashboard/Dashboard.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import styles from "./Dashboard.module.css"
+
+import Logo from '../../components/Logo/Logo'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <Logo/>
+      </div>
+      <div className={styles.right}>
+        ah
+      </div>
+    </div>
   )
 }
