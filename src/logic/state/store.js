bd473170396ec7cbb410bc83bdbbf7ecee74ca62
Adistian Herlambang <adistian59@gmail.com>
2026-05-12 16:26:57 +0700

    zustand init

---
+++ src/logic/state/store.js
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export const userStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      login: (data) => set({ user: data }),
+      logout: () => set({user: null})
+    })
+  )
+)
