1a7e1e6b613a2ee81f0003c5accd019843dbeddf
Adistian Herlambang <adistian59@gmail.com>
2026-06-06 16:29:48 +0700

    ok dummy

--- src/page/Dashboard/Workspace/HistoriWorkspace.jsx
+++ src/page/Dashboard/Workspace/HistoriWorkspace.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import styles from "./HistoriWorkspace.module.css"
 
+//component
+import Wrapper from '../../../components/Wrapper/Wrapper'
+import HitoriAnalisis from '../../../components/FromPage/Overview/HitoriAnalisis'
+
 export default function HistoriWorkspace() {
   return (
     <div className={styles.container}>
       <p className={styles.tabTitle}>Histori Workspace</p>
 
-
+      <div className={styles.containerr}>
+        <div className={styles.wrapper}>
+          <Wrapper><HitoriAnalisis /></Wrapper>
+        </div>
+      </div>
     </div>
   )
 }
