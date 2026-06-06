b06a974ef7d58eb94772a9037a3b9223e690057e
Adistian Herlambang <adistian59@gmail.com>
2026-06-06 19:57:21 +0700

    analisis module css

--- src/page/Dashboard/Workspace/Analisis.jsx
+++ src/page/Dashboard/Workspace/Analisis.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import styles from "./Analisis.module.css"
+
+import Wrapper from '../../../components/Wrapper/Wrapper'
 
 export default function Analisis() {
+
   return (
-    <div>Analisis</div>
+    <div>
+      <Wrapper>asdia</Wrapper>
+    </div>
   )
 }
