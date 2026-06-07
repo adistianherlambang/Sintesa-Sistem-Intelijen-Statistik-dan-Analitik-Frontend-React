c568181e34dcab41f9adbb1c499490c82d7c02b8
Adistian Herlambang <adistian59@gmail.com>
2026-06-08 03:23:33 +0700

    main button

--- src/components/MainButton/MainButton.jsx
+++ src/components/MainButton/MainButton.jsx
@@ -2,6 +2,17 @@ import { useState, useEffect } from "react";
 
 export default function MainButton({ children, onClick }) {
   return (
-    <div onClick={onClick}>{children}</div>
+    <div onClick={onClick} style={{
+      backgroundColor: "#34B34A",
+      color: "white",
+      height: "32px",
+      borderRadius: "4px",
+      display: "flex",
+      gap: "0.5rem",
+      justifyContent: "center",
+      alignItems: "center",
+      cursor: "pointer",
+      padding: "4px 1rem"
+    }}>{children}</div>
   )
 }
