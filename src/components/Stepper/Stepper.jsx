83eaa9bd25975d64909c82dcbda95762e46b194f
Adistian Herlambang <adistian59@gmail.com>
2026-06-08 00:45:17 +0700

    stepper

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import styles from "./Stepper.module.css"
 
-export default function Stepper({ }) {
+export default function Stepper({ item }) {
+
+  const [step, setStep] = useState(0)
+  const CurrentContent = item[step].content
+
   return (
     <div>
-
+      <div>{step}</div>
+      <CurrentContent setStep={setStep} />
     </div>
   )
 }
--- src/page/Dashboard/Workspace/Analisis.jsx
+++ src/page/Dashboard/Workspace/Analisis.jsx
@@ -2,23 +2,61 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import styles from "./Analisis.module.css"
 
+// component
+import Stepper from '../../../components/Stepper/Stepper'
 import Wrapper from '../../../components/Wrapper/Wrapper'
 
 export default function Analisis() {
+
+  const [state, setState] = useState("")
+
+  const item = [
+    { label: "StepOne", content: StepOne },
+    { label: "StepTwo", content: StepTwo },
+    { label: "StepThree", content: StepThree },
+  ]
 
   return (
     <div className={styles.container}>
       <p className={styles.tabTitle}>Analisis</p>
       <Wrapper>asdia</Wrapper>
-      <StepOne />
+      <Stepper item={item} />
     </div>
   )
 }
 
-function StepOne() {
+function StepOne(props) {
+
+  const { setStep } = props
+
   return (
     <div className={styles.container}>
       <Wrapper>satu</Wrapper>
+      <div onClick={() => setStep(1)}>klik</div>
+    </div>
+  )
+}
+
+function StepTwo(props) {
+
+  const { setStep } = props
+
+  return (
+    <div className={styles.container}>
+      <Wrapper>dua</Wrapper>
+      <div onClick={() => setStep(2)}>klik</div>
+    </div>
+  )
+}
+
+function StepThree(props) {
+
+  const { setStep } = props
+
+  return (
+    <div className={styles.container}>
+      <Wrapper>tiga</Wrapper>
+      <div onClick={() => setStep(0)}>klik</div>
     </div>
   )
 }
