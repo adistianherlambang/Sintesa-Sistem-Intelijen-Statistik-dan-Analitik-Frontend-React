820924c9a35f101ad7def142cc99c64880a02826
Adistian Herlambang <adistian59@gmail.com>
2026-06-08 12:55:10 +0700

    hampir semua tentang akun

--- src/page/Dashboard/Akun/TentangAkun.jsx
+++ src/page/Dashboard/Akun/TentangAkun.jsx
@@ -6,12 +6,15 @@ import Wrapper from "../../../components/Wrapper/Wrapper";
 import Input from "../../../components/Input/Input";
 
 export default function TentangAkun() {
+
+  const tanggalBergabungDummy = "2026-06-09";
+
   return (
     <div className={styles.container}>
       <p className={styles.tabTitle}>Tentang Akun</p>
-      <div className={styles.wrapper}>
-        <div className={styles.section}>
-          <Wrapper>
+      <div className={styles.section}>
+        <Wrapper>
+          <div className={styles.content}>
             <p className={styles.sectionTitle}>Informasi Instansi</p>
             <div className={styles.inputContainer}>
               <div className={styles.inputWrapper}>
@@ -27,8 +30,49 @@ export default function TentangAkun() {
                 <Input type={"text"} />
               </div>
             </div>
-          </Wrapper>
-        </div>
+            <p style={{
+              color: "#AAAAAA",
+              fontSize: "12px"
+            }}>Tanggal bergabung: {tanggalBergabungDummy}</p>
+          </div>
+        </Wrapper>
+      </div>
+      <div className={styles.section}>
+        <Wrapper>
+          <div className={styles.content}>
+            <p className={styles.sectionTitle}>Informasi PIC</p>
+            <div className={styles.inputContainer}>
+              <div className={styles.inputWrapper}>
+                <p>Nama PIC</p>
+                <Input type={"text"} />
+              </div>
+              <div className={styles.inputWrapper}>
+                <p>Email</p>
+                <Input type={"text"} />
+              </div>
+              <div className={styles.inputWrapper}>
+                <p>Kontak Utama</p>
+                <Input type={"text"} />
+              </div>
+            </div>
+            <p style={{
+              color: "#AAAAAA",
+              fontSize: "12px"
+            }}>Tanggal bergabung: {tanggalBergabungDummy}</p>
+          </div>
+        </Wrapper>
+      </div>
+      <div className={styles.section}>
+        <Wrapper>
+          <div className={styles.content}>
+            <p className={styles.sectionTitle}>Keamanan Akun</p>
+          </div>
+        </Wrapper>
+        <Wrapper>
+          <div className={styles.content}>
+            <p className={styles.sectionTitle}>Kelola Session</p>
+          </div>
+        </Wrapper>
       </div>
     </div>
   )
