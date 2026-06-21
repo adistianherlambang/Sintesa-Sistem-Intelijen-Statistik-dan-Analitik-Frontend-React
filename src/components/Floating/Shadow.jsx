dcdb2f3f776796851197470e2dced4c3a91e71b8
Adistian Herlambang <adistian59@gmail.com>
2026-06-21 07:49:52 +0700

    style: enhance Shadow component with glare layered SVGs and saturation backdrop-filter

--- src/components/Floating/Shadow.jsx
+++ src/components/Floating/Shadow.jsx
@@ -8,6 +8,7 @@ export default function Shadow() {
 
   return (
     <div className={styles.container}>
+      <div className={styles.saturationLayer}></div>
       <svg className={styles.svg}
         viewBox="0 0 1164 831" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M852.404 0.000448161C1119.35 315.577 1235.69 620.258 1117.52 758.502C963.421 938.775 467.698 769.179 -0.000322897 379.811L-0.000255846 0.000409813L852.404 0.000448161Z" fill="url(#paint0_radial_1165_429)" fill-opacity="0.1" />
